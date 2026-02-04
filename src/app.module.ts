cbad4cf2294824079b269ee6ce808ee2c1b2bd19
BharateshPoojary <bharateshpoojari@gmail.com>
2026-02-04 22:56:44 +0530

    Defining Schema in progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(''),
-  ], //environment variable key/value pairs are parsed and resolved loaded in process.env adnd now   The forRoot() method registers the ConfigService provider, which provides a get() method for reading these parsed/merged configuration variables.
+    ConfigModule.forRoot({ isGlobal: true }), //environment variable key/value pairs are parsed and resolved loaded in process.env adnd now   The forRoot() method registers the ConfigService provider, which provides a get() method for reading these parsed/merged configuration variables.
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/types/document.type.ts
@@ -0,0 +1,29 @@
+export interface BankStatement {
+  fileName: string;
+  bankName: string;
+  accountHolderName: string;
+  accountNumber: string;
+  accountType?: string;
+  currency: string;
+  statementStartDate: string;
+  statementEndDate: string;
+  openingBalance: number;
+  closingBalance: number;
+  transactions: Transaction[];
+}
+
+export interface Transaction {
+  date: string;
+  description: string;
+  debitAmount: number | null;
+  creditAmount: number | null;
+  runningBalance: number;
+}
+
+export interface BankStatementDocument extends Document {
+  jobId: string;
+  data: BankStatement[];
+  status: 'PROCESSING' | 'COMPLETED' | 'FAILED';
+  createdAt: Date;
+  updatedAt: Date;
+}
