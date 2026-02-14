b077ee8f9ef1affaa024679ad9169f5cc1137940
BharateshPoojary <bharateshpoojari@gmail.com>
2026-02-14 15:51:13 +0530

    refactor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { text } from 'express';
 
 async function bootstrap() {
   const app: INestApplication =
@@ -9,6 +10,7 @@ async function bootstrap() {
       abortOnError: false,
       cors: true,
     });
+  app.use('/notify', text({ type: 'text/plain' }));
   await app.listen(3001);
 }
 bootstrap();
