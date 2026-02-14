e75d4a63b984046846e3a667e5aa5fdd034b1eec
BharateshPoojary <bharateshpoojari@gmail.com>
2026-02-14 15:56:05 +0530

    deps added

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication } from '@nestjs/common';
-// import { NestExpressApplication } from '@nestjs/platform-express';
-// import { text } from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { text } from 'express';
 
 async function bootstrap() {
   const app: INestApplication =
-    await NestFactory.create(AppModule, {
+    await NestFactory.create<NestExpressApplication>(AppModule, {
       abortOnError: false,
       cors: true,
     });
-  // app.use('/notify', text({ type: 'text/plain' }));
+  app.use('/notify', text({ type: 'text/plain' }));
   await app.listen(3001);
 }
 bootstrap();
